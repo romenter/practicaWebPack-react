258afe12a72fc51b264d38511c9acff0286b936c
Romen, Ter Gazarian <romen.tergazarian@gmail.com>
2022-04-11 18:45:43 -0300

    subiendo proyecto de aprendizaje React

---
+++ src/components/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+//Sintaxis actual para definir componentes React
+const App = () => <h1>Hello React! APARECE CONCHUDO</h1>  
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import App from './components/App.jsx';
+import './styles/global.scss'
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
